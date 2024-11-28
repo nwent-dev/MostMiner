28d53d7c65f68d29cd8cdcf278f81ff39a449819
nwent-dev <nwent@proton.me>
2024-11-28 15:44:56 +0300

    Initial Commit

---
+++ MostMiner/MostMinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostMinerApp.swift
+//  MostMiner
+//
+//  Created by Александр Калашников on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MostMinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
